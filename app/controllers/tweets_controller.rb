72874c8604b31016d6faeb54eb416c577028fe77
Alex Howington <alex.howington@gmail.com>
2014-07-23 16:35:15 -0600

    Add tweets#index action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,25 @@ class TweetsController < ApplicationController
   before_action :require_signin
 
   def index
+    uri = URI("https://api.twitter.com")
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+
+    @query = if params[:query]
+      Query.create!(content: params[:query])
+      URI::encode(params[:query])
+    else
+      URI::encode(['stacksocial', 'ruby on rails', 'tenderlove', 'railscasts'].sample)
+    end
+
+    request = Net::HTTP::Get.new("/1.1/search/tweets.json?q=#{@query}&count=20&result_type=recent")   
+    request["Authorization"] = "Bearer #{ENV['BEARER_TOKEN']}"
+    
+    response = https.request(request)
+
+    @tweets = JSON.parse(response.body)["statuses"]
+
+    @query = URI::decode(@query)
   end
-  
+
 end
