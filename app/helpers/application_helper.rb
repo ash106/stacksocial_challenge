338d53d06ea6b773aa460505d809af300f6392b9
Alex Howington <alex.howington@gmail.com>
2014-07-23 16:25:08 -0600

    Add get_bearer_token helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+  def get_bearer_token
+    encoded_key = URI::encode ENV["TWITTER_KEY"]
+    encoded_secret = URI::encode ENV["TWITTER_SECRET"]
+    encoded_credentials = Base64.strict_encode64("#{encoded_key}:#{encoded_secret}".force_encoding('UTF-8'))
+
+    uri = URI("https://api.twitter.com/oauth2/token")
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Post.new(uri.path)
+    request["Authorization"] = "Basic #{encoded_credentials}"
+    request["Content-Type"] = 'application/x-www-form-urlencoded;charset=UTF-8'
+    request.body = "grant_type=client_credentials"
+
+    response = https.request(request)
+
+    results = JSON.parse(response.body)
+
+    if results["token_type"] == "bearer"
+      results["access_token"]
+    else
+      results["errors"]
+    end
+  end
+  
 end
