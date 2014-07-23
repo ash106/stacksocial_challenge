0c6f41833365dbee94c0f1d1f7ec5972b8119780
Alex Howington <alex.howington@gmail.com>
2014-07-23 16:20:35 -0600

    Clean up pages using Bootstrap classes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if user = User.authenticate(params[:email], params[:password])
+    if user = User.authenticate(params[:session][:email], params[:session][:password])
       session[:user_id] = user.id
       flash[:notice] = "Welcome back, #{user.email}!"
       redirect_to session[:intended_url] || root_path
